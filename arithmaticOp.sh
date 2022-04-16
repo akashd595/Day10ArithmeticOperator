3bc63bcfa9d48cd6703ed27b627e501507976866
Akash Das <akashd595@gmail.com>
2022-04-16 15:53:17 +0530

    Added: input 3 numbers

--- arithmaticOp.sh
+++ arithmaticOp.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -x
 echo "This problem computes different arithmetic expressions and Sorts the results"
+read -p "Enter 1st number " a;
+read -p "Enter 2nd number " b;
+read -p "Enter 3rd number " c;
+
