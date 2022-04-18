84e5f8c752e15172bb1a429d50f4e26f216674cf
Akash Das <akashd595@gmail.com>
2022-04-18 12:18:17 +0530

    Added: Store data in an Array

--- arithmaticOp.sh
+++ arithmaticOp.sh
@@ -3,9 +3,17 @@ echo "This problem computes different arithmetic expressions and Sorts the resul
 read -p "Enter 1st number " a;
 read -p "Enter 2nd number " b;
 read -p "Enter 3rd number " c;
-
-firstOp=`echo "$a+$b*$c"|bc -l`;
-secondOp=`echo "$a*$b+$c"|bc -l`;
-thirdOp=`echo "$c+$a/$b"|bc -l`;
-fourthOp=`echo "$a%$b+$c"|bc -l`
-echo $fourthOp;
+declare -A CompDic
+CompDic["a+b*c"]=`echo "$a+$b*$c"|bc -l`;
+CompDic["a*b+c"]=`echo "$a*$b+$c"|bc -l`;
+CompDic["c+a/b"]=`echo "$c+$a/$b"|bc -l`;
+CompDic["a%b=c"]=`echo "$a%$b+$c"|bc -l`;
+echo "Dic : "${!CompDic[@]};
+echo "Dic : "${CompDic[@]};
+n=0
+for key in "${!CompDic[@]}"
+do
+	Array_com[(($n))]=${CompDic[$key]}
+	((n++))
+done
+echo "Array: " ${Array_com[@]}
